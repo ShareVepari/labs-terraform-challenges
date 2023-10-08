3ab589efa3078e94a8ff7cc59bddf427302ce9bb
Share Vepari <sharevepari@gmail.com>
2023-10-07 21:45:54 -0700

    Challenge-1 completed

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,17 +1,37 @@
 provider "aws" {
-  version = "~> 2.54"
-  region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+ //version = "~> 2.54"
+ region = "us-west-2"
 }
 
-provider "digitalocean" {}
+# provider "digitalocean" {}
+
+// Following did not work
+/*
+provider "digitalocean" {
+  alias = "digitalocean"
+  source = "digitalocean/digitalocean"
+}
+*/  //Did not work
 
 terraform {
-    required_version = "0.12.31"
+//  required_version = "~> 1.5"
+
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+     // version = "2.30.0"
+    }
+  }
+// Did not work as you cannot have 2 providers
+  /*
+  required_providers  {
+  aws = {
+    version = "~> 2"
+  }
+  */
 }
 
 
-resource "aws_eip" "kplabs_app_ip" {
-  vpc      = true
+resource "aws_iam_user" "iam_user" {
+  name = "labs-test"
 }
